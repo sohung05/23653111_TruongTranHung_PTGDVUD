c12d29959c6e67d0f8108743d406062884e5b802
sohung05 <23653111.hung@student.iuh.edu.vn>
2026-04-05 16:50:36 +0700

    Bai tap tuan 6

---
+++ Week6/bai1-router/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
